37b50c69bb41b86776bd89d1e13c4491005e03a9
joao <joao.brito.ramos@gmail.com>
2015-01-19 11:09:33 +0000

    Commit to WebPage

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,8 @@ module.exports = function(grunt) {
                 cwd: '<%= appfolder.app %>',
                 src: ['lib/**', 'node_modules/angular/angular.min.js',
                      'node_modules/angular/angular.min.js.map',
-                     'node_modules/angular-ui-router/release/angular-ui-router.min.js'],
+                     'node_modules/angular-ui-router/release/angular-ui-router.min.js',
+                     'src/package.json'],
                 dest: '<%= appfolder.dist %>'
             }]
         }
