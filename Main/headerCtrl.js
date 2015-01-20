6fca13bbad92a6aea67925eba24793f6abe9bade
joao <joao.brito.ramos@gmail.com>
2015-01-20 12:17:52 +0000

    Admin Modules

--- Main/headerCtrl.js
+++ Main/headerCtrl.js
@@ -2,28 +2,6 @@
 
 
   angular.module('Myapp').controller('headerCtrl', function ($scope, authtoken, $rootScope) {
-    
- 
-    $scope.isAuthenticated=function(){
-       return authtoken.getData();
-    };
-    $scope.ver=false; 
-    
- $scope.$watch(function () { return authtoken.getData; },
-                function (value) {
-                    console.log("In $watch - bla:" + value() + "-" + $scope.teste );
-                   
-                    if (value() == true){
-                        $scope.teste = true;
-                        
-                    }else{
-                        $scope.teste = null;
-                    }
 
-                }
-            );      
-      
-  $scope.teste= false;
-    
-      
+      $scope.isAuthenticated= authtoken.getData;
   });
