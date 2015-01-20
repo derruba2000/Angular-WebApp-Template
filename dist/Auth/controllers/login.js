e3d3d57e9b09d3d1f88a3057d67d80d85b0e0816
joao <joao.brito.ramos@gmail.com>
2015-01-20 15:56:40 +0000

    Version 0.1.0

--- dist/Auth/controllers/login.js
+++ dist/Auth/controllers/login.js
@@ -1,17 +1,11 @@
 'use strict';
 
 
-  angular.module('Myapp.Auth').controller('loginCtrl', function ($scope, authtoken,$rootScope,$state) {
-
+  angular.module('Myapp.Auth').controller('loginCtrl', function ($scope, authtoken,$rootScope,$state, alert) {
 
     $scope.Authenticate = function(){
         $rootScope.authenticated=true;
          authtoken.setData(true);
+         alert('success', 'Hi!', 'Welcome back user 1');
     };
-    $scope.Logout = function(){
-        $rootScope.authenticated=false;
-        authtoken.setData(false);
-        $state.go('home');
-    };
-    
   });
--- dist/Auth/controllers/logout.js
+++ dist/Auth/controllers/logout.js
@@ -1,15 +1,12 @@
 'use strict';
 
 
-  angular.module('Myapp.Auth').controller('logoutCtrl', function ($scope, authtoken,$rootScope,$state) {
+  angular.module('Myapp.Auth').controller('logoutCtrl', function ($scope, authtoken,$rootScope,$state, alert) {
   
-      if (!authtoken.getData()){
-         $state.go('home');    
-      }
-      
       $rootScope.authenticated=false;
       authtoken.setData(false);
       authtoken.removeData();
+      alert('default', 'Good bye!', 'See you later user 1');
       $state.go('home');
-
+     
   });
